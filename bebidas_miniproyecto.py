ab2cdf6ab77e2f28433c5ff89c100336a3147b41
GabiPoko <gabipokorasky@gmail.com>
2024-08-21 15:34:02 -0300

    actualizacion de código

--- bebidas_miniproyecto.py
+++ bebidas_miniproyecto.py
@@ -1,3 +1,8 @@
+
+# Intente crear una instancia de clase con los atributos bebida, precio e ingredientes
+# se puede crear tantos objetos como bebidas haya en el menu y despues agregar mas
+
+
 class BebidasMenu:
 
     def __init__(self, bebida, precio, ingredientes):
@@ -9,4 +14,12 @@ class BebidasMenu:
         print (f' {self.bebida}: {self.precio}\n Ingredientes: {self.ingredientes}')
 
 campari = BebidasMenu('Campari', 3500, 'Naranja, aguardiente y jarabe azucarado')
-campari.mostrar_bebida()
+# campari.mostrar_bebida()
+
+class BebidasSinAlcohol (BebidasMenu):
+
+ limonada = BebidasMenu('Limonada', 2000, 'Limón, azucar, agua')
+
+ limonada.mostrar_bebida()
+
+    
